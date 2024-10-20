8f02393de41aa7c46a2ea9ff50eab508f7947d5d
Venkatm1182 <venkatm1182@gmail.com>
2024-10-20 21:10:44 +0530

    Final commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 6381697754 </p>
+         <p> <i class="fas fa-phone"></i> +91 8270726443 </p>
+         <p> <i class="fas fa-envelope"></i> venkatm1182@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Coimbatore, India </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/venkatesh.murugan.94651" target="_blank"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://twitter.com/venky_nerd" target="_blank"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/_when.cut.ace_/" target="blank"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/venkatesh-m-607b45244/" target="_blank"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; Copyright  @ <?php echo date('Y'); ?> by <span>Venky  <i class="fa-solid fa-heart"></i></span> </p>
+
+</section>
